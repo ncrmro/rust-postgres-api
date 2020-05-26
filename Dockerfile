17999b74835426f9d533feec4882a9430e3ca348
ncrmro <ncrmro@gmail.com>
2020-05-26 16:23:46 -0500

    feat: add support for using listner

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM rust:1.43 as watcher
 WORKDIR /app
 EXPOSE 8000
 
-RUN cargo install cargo-watch
+RUN cargo install cargo-watch systemfd
 
-CMD ["cargo", "watch", "-x", "run"]
+CMD ["systemfd", "--no-pid", "-s", "http::0.0.0.0:8000", "--", "cargo", "watch", "-x", "run"]
--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,3 +1,5 @@
+use listenfd::ListenFd;
+use std::env;
 use actix_web::{get, App, HttpServer, Responder};
 
 #[get("/")]
@@ -6,8 +8,21 @@ async fn index() -> impl Responder {
 }
 
 pub async fn server() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("0.0.0.0:8000")?
+    // this will enable us to keep application running during recompile: systemfd --no-pid -s http::5000 -- cargo watch -x run
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| App::new().service(index));
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("HOST is not set in .env file");
+            let port = env::var("PORT").expect("PORT is not set in .env file");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    server
         .run()
         .await
 }
