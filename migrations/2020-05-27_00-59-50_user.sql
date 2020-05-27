53555996dccb42df82bb03276e9b7cab65820402
ncrmro <ncrmro@gmail.com>
2020-05-26 20:12:05 -0500

    chore: check in initial migrations

---
+++ migrations/2020-05-27_00-59-50_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    bio VARCHAR(255),
+    image VARCHAR(2048),
+    token VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
