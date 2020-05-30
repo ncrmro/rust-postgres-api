46c57627a251ddf9bfab707f8407b6786c281983
ncrmro <ncrmro@gmail.com>
2020-05-30 07:46:57 -0500

    test(index): assert root is hello world

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,12 @@
 extern crate planet_express;
 mod common;
 
-use actix_web::test;
+use actix_web::test::{read_response, TestRequest};
 
 #[actix_rt::test]
 async fn test_index_get() {
-    let mut server = common::server().await;
-    let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
-    let resp = test::call_service(&mut server, req).await;
-    assert!(resp.status().is_success());
+    let mut srv = common::server().await;
+    let req = TestRequest::with_header("content-type", "text/plain").to_request();
+    let res = read_response(&mut srv, req).await;
+    assert_eq!(res, "Hello World".as_bytes());
 }
