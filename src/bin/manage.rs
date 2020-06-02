82be3a70974052bb134cf873cc892324c567e2e6
ncrmro <ncrmro@gmail.com>
2020-06-02 16:16:36 -0500

    chore(manage): clean up

--- src/bin/manage.rs
+++ src/bin/manage.rs
@@ -55,17 +55,6 @@ fn main() {
 
     let opts: Opts = Opts::parse();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match opts.verbose {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
     match opts.subcmd {
         SubCommand::Createuser(args) => block_on(create_user(args, settings)),
     }
