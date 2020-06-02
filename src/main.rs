c42f1e07c67382474613781a7c7a696832a58ef1
ncrmro <ncrmro@gmail.com>
2020-06-02 10:29:36 -0500

    fix(ci): split commands and only build for now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 extern crate fake;
 extern crate planet_express;
 use anyhow::Result;
+use std::env;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[0];
+    // let filename = &args[2];
+
+    println!("Searching for {}", query);
+    // println!("In file {}", filename);
+
     planet_express::init()
 }
